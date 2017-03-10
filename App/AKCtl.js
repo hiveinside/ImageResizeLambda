aa86920936dc6ca6355a149ac6722316007a47e8
Anshu <anshu.mishra@xolo.in>
2017-03-10 21:06:31 +0530

    Image downlod via lambda

--- App/AKCtl.js
+++ App/AKCtl.js
@@ -6,7 +6,7 @@ var  request = require('request'),
     async_parallel = require('async-parallel'),
     query = require("pg-query");
 
-var AKCtl = {baseURL:function(url){return "https://53unhwyitj.execute-api.us-east-1.amazonaws.com/prod/ImageDownloaderViaNodeJS?imageurl="+url,imageResizeBaseUrl='http://lavaimagerepo.s3-website-us-west-2.amazonaws.com'}};
+var AKCtl = {baseURL:function(url){return "https://53unhwyitj.execute-api.us-east-1.amazonaws.com/prod/ImageDownloaderViaNodeJS?imageurl="+url},imageResizeBaseUrl:'http://lavaimagerepo.s3-website-us-west-2.amazonaws.com'};
 
 
 AKCtl.configApp = function (CONFIG) {
@@ -31,10 +31,13 @@ AKCtl.init = function () {
             return false;
         }
 
-        query('select id,img_hdpi,template from feeds where id>'+rows[0].feed_id+'order by id asc', function(err, rows) {
+
+        query("select id,img_hdpi,template from feeds where id>"+rows[0].feed_id+' order by id asc', function(err, rows) {
+
+
 
             async_parallel.concurrency = 100;
-
+                        
             async_parallel.each(rows, function (row) {
                 var imageEncodedURL=AKCtl.baseURL(encodeURIComponent(row.img_hdpi));
 
@@ -46,21 +49,25 @@ AKCtl.init = function () {
                         imagenameArr=row.img_hdpi.split('\/');
                         imagename=imagenameArr[imagenameArr.length-1];
 
-                         if(template=='fullcard')   
-                           {url=imageResizeBaseUrl+'/0x168/'+imagename;}     
+                         if(row.template=='fullcard')   
+                           {
+                            url=AKCtl.imageResizeBaseUrl+'/0x168/'+imagename;
+                        }     
 
                         else{
-                            url=imageResizeBaseUrl+'/112x114/'+imagename;
+                            url=AKCtl.imageResizeBaseUrl+'/112x114/'+imagename;
                         }
 
                      request(url, function (error, response, body) {
                         if (!error && response.statusCode == 200) {
-                            console.log(body);
+                            console.log('Its working '+url);
                         }
 
 
-                    }
+                    
                 });
+
+                 }
 
             });
 
@@ -79,6 +86,8 @@ AKCtl.init = function () {
     });
 
 
+
+});
     /***
      * select * from lambdaconfig
      * select img_hdpi from feeds order by id desc limit 1
--- index.js
+++ index.js
@@ -13,3 +13,12 @@ exports.handler = function index(event, context) {
     }).init();
 };
 
+
+
+
+    AKCtl.configApp({
+        "HOST": 'hivelauncherrds2.ciw9asb47tat.us-east-1.rds.amazonaws.com',
+        "USERNAME":'test',
+        "PASSWORD": 'Hive2013',
+        "DATABASE": 'minusone'
+    }).init();
